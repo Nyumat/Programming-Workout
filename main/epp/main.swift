e677c085748ea650a2f3d3947ae2b6964a7d8939
thomas <46255836+Nyumat@users.noreply.github.com>
2020-08-15 02:49:41 -0400

    Update main.swift

--- main/epp/main.swift
+++ main/epp/main.swift
@@ -127,57 +127,113 @@ func fizzBuzz() {
 fizzBuzz()
  
 
-
  // HackerEarth's first problem.
+
 
 let num = Int(readLine(strippingNewline: true)!)
 let str = readLine()
 let newN = num! * 2
 print("\(newN)" + "\n\(str!)")
  
- 
-
-// Traverse an array of i size, with an array of random numbers, and count each occurance of N number. */
 
 
-func travserseArray(value:Int,i:Int,range:Int){
-    var counter = 0
-    var arr = [Int]()
-    var values = [Int]()
+// Traverse an unsorted array of "arr_size" random numbers of "num_range" range and for "search_amount" values.
+
+func traverseArray(arr_size:Int,numRange:Int,searchAmount:Int){
+
+var arr = [Int]()
+var values = [Int]()
+var count = 0
     
-    for _ in 1...3 {
-        print("Add numbers to look for.")
-        let valuesInput = Int(readLine(strippingNewline: true)!)
-        values.append(valuesInput!)
+    for _ in 0..<searchAmount {
+    print("Add values to look for.")
+    let valuesInput = Int(readLine(strippingNewline: true)!)
+    values.append(valuesInput!)
     }
     
-    for _ in 1...i {
-            let um = Int.random(in: 0...i)
-            arr.append(um)
-       }
+    for _ in 0..<arr_size{
+        let randomNumber = Int.random(in: 1...numRange)
+        arr.append(randomNumber)
+    }
     
     for num in arr {
-        for v in values {
-            if values[v] == arr[num] {
-                counter = counter + 1
-            } else {
-                continue
+        for v in (0..<values.count).reversed() {
+                if num == values[v] {
+                    count = count +  1
+                } else {
+                    continue        
+                }
             }
-        }
-    }
-    switch counter {
-    case let counter where  counter == 0:
-            print("There are no occurances  of " + "\(values[value]) in  this array.")
-            break
-    case let counter  where counter >= 1:
-            print("There are " + "\(counter)" +  " occurances of " + "\(values[value])")
-    default:
-        print("null")
     }
     print(arr)
+    if count >= 0 {
+    print("There are " + " \(count) instances of "  +  " \(values)")
+    }
 }
+    
+traverseArray( arr_size: 100, numRange: 100,searchAmount: 10)
 
-travserseArray(value: 3,i:3,range:10)
+ 
+/*
+INPUT:
+Add values to look for.
+10
+Add values to look for.
+20
+Add values to look for.
+30
+Add values to look for.
+40
+Add values to look for.
+50
+Add values to look for.
+60
+Add values to look for.
+70
+Add values to look for.
+80
+Add values to look for.
+90
+Add values to look for.
+100
+
+OUTPUT:
+[31, 100, 74, 80, 50, 86, 65, 36, 37, 47, 76, 5, 2, 95, 85, 21, 52, 29, 68, 11, 1, 55, 69, 84, 4, 55, 83, 11, 80, 73, 98, 23, 46, 25, 47, 3, 83, 37, 6, 44, 27, 71, 75, 70, 57, 10, 40, 65, 91, 1, 45, 29, 31, 60, 13, 29, 69, 55, 34, 83, 75, 73, 80, 50, 70, 100, 14, 70, 89, 95, 58, 92, 53, 24, 79, 2, 28, 48, 87, 62, 37, 37, 55, 73, 98, 82, 15, 64, 48, 31, 81, 80, 18, 31, 63, 12, 91, 19, 75, 15]
+There are  14 instances of [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+*/
+
+// Non-multiple N search solution
+
+// Traverse an array of "arr_size" size, containing random numbers, and count each occurance of N number. */
+
+func traverseArray(number:Int,arr_size:Int,numRange:Int){
+
+var arr = [Int]()
+var count = 0
+    
+    for _ in 0..<arr_size{
+        let randomNumber = Int.random(in: 1...numRange)
+        arr.append(randomNumber)
+    }
+    
+    for num in arr {
+        if num == number {
+            count = count +  1
+        }
+    
+    }
+    print(arr)
+    print("There are " + " \(count) instances of "  +  " \(number)")
+    
+}
+traverseArray(number: 1, arr_size: 50, numRange: 50)
+
+/*
+
+Output : [46, 48, 37, 10, 28, 1, 6, 33, 42, 15, 49, 46, 41, 20, 35, 49, 43, 2, 15, 17, 10, 29, 39, 35, 45, 8, 2, 33, 33, 16, 2, 11, 19, 33, 28, 15, 46, 12, 33, 47, 36, 35, 21, 41, 30, 7, 40, 16, 2, 38]
+There are  1 instances of  1
+O(n) time complexity
+*/
 
 /*
  You are given a string  S , which comprises English alphabets, determine the count of all the vowels in  S . Vowels are  [ a , e , i , o , u ] . Print the count of all the vowels that are available in the string  S .
@@ -219,4 +275,5 @@ func findVowels(word:String){
 }
 findVowels(word: "Hello! My name is Thomas.")
 
+// Input: "Hello! My name is Thomas."
 // Output : There are 7 vowels
