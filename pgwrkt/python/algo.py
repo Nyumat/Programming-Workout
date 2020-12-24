eec5f78c7d140b37736c8ab2bce77f09f40693fc
Thomas <nyumat18@gmail.com>
2020-12-24 01:45:56 -0500

    Update algo.py

--- pgwrkt/python/algo.py
+++ pgwrkt/python/algo.py
@@ -291,7 +291,6 @@ board_obj = makeGrid(10,10)
 
 printt(board_obj)
 
-"""
 def findNthOccuring(string,target,char):
     occur = 0
 
@@ -305,3 +304,31 @@ def findNthOccuring(string,target,char):
 print(findNthOccuring([1,2,1,1,3,1,2,1],5,1))
 
 # Output: 7 TC : Linear SC: O(n)
+
+However, we should try and aim for the optimal solution,
+which is done through binary search!
+
+"""
+# Basic Binary Search 
+
+def binarySearch(nums,target):
+    l,r = 0,len(nums) - 1
+
+    while l <= r:
+        mid = l + (r - l) // 2
+        if target == nums[mid]:
+            return mid
+        elif target < nums[mid]:
+            r = mid - 1
+        else:
+            l = mid + 1
+    
+    return -1
+
+nums = [2,4,5,6,8,9,10,11]
+target = 4
+
+index = binarySearch(nums,target)
+
+print("Target was found at index:",index)
+# Output Tagret was found at index: 1
